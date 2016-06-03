47e49d2f6867c5970ca535d8bafb9ef699ea4201
leo <killerofgnom@gmail.com>
2016-06-03 13:44:01 +0200

    Exec modifié

--- Planingfleur69/src/presentation/Exec.java
+++ Planingfleur69/src/presentation/Exec.java
@@ -22,19 +22,32 @@ public class Exec {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		
 		Employe e = Factory.getEmploye();
+		Employe e1 = Factory.getEmploye();
 		Materiel m = Factory.getMateriel();
+		Materiel m1 = Factory.getMateriel();
 		Tache t = pu.addTache(e, "debrouissailler", "Allee des noisetiers", sdf.parse("2016-06-03"), 1);
 		
-		eu.modifEmploye(e, "Wallace", "William");
+		//eu.modifEmploye(e, "Wallace", "William");
+		eu.modifEmploye(e1, "Rabbit", "Roger");
 		mu.modifMateriel(m, "scie cirulaire");
+		mu.modifMateriel(m1, "rotofil");
 		
+		t.setIdTache(1);
 		e.setId(1);
-		m.setIdMateriel(1);
-		
+		m.setIdMateriel(1);		
 		
 		System.out.println(e);
 		System.out.println(m);
 		System.out.println(t);
+		
+		pu.associerMatos(t, m);
+		pu.associerMatos(t, m1);
+		
+		System.out.println(t.getLstMatos());
+		
+		pu.associerEmploye(t, e1);
+		
+		System.out.println(t.getLstEmploye());
 		
 		
 		
