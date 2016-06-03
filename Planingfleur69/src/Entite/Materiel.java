6a06082725802a0f81d92be9382026ab64ce57ab
Berengere <berengere.pelletier@gmail.com>
2016-06-03 11:36:19 +0200

    creation classe materiel

---
+++ Planingfleur69/src/Entite/Materiel.java
@@ -0,0 +1,39 @@
+package Entite;
+
+public class Materiel {
+	
+	int idMateriel;
+	String type;
+	boolean etat;
+	public int getIdMateriel() {
+		return idMateriel;
+	}
+	public void setIdMateriel(int idMateriel) {
+		this.idMateriel = idMateriel;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public boolean isEtat() {
+		return etat;
+	}
+	public void setEtat(boolean etat) {
+		this.etat = etat;
+	}
+	public Materiel(int idMateriel, String type, boolean etat) {
+		super();
+		this.idMateriel = idMateriel;
+		this.type = type;
+		this.etat = etat;
+	}
+	@Override
+	public String toString() {
+		return "Materiel [idMateriel=" + idMateriel + ", type=" + type + ", etat=" + etat + "]";
+	}
+	
+	
+
+}
