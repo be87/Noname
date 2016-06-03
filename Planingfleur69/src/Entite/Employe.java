e34de6246074af1115e729c40f7ee45a25499d7c
Berengere <berengere.pelletier@gmail.com>
2016-06-03 11:31:40 +0200

    creation classe employe

--- Planingfleur69/src/Entite/Employe.java
+++ Planingfleur69/src/Entite/Employe.java
@@ -2,4 +2,54 @@ package Entite;
 
 public class Employe {
 
+	int id;
+	String nom;
+	String prenom;
+	boolean etat;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public boolean isEtat() {
+		return etat;
+	}
+
+	public void setEtat(boolean etat) {
+		this.etat = etat;
+	}
+
+	public Employe(int id, String nom, String prenom, boolean etat) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.etat = etat;
+	}
+
+	@Override
+	public String toString() {
+		return "Employe [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", etat=" + etat + "]";
+	}
+
 }
