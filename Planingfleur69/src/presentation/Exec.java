93caced3c4d4df264fb0c0212f6179c9b2ad9d85
leo <killerofgnom@gmail.com>
2016-06-03 13:24:50 +0200

    suite exec test

--- Planingfleur69/src/presentation/Exec.java
+++ Planingfleur69/src/presentation/Exec.java
@@ -1,29 +1,40 @@
 package presentation;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import Entite.Employe;
 import Entite.Factory;
 import Entite.Materiel;
 import Entite.Tache;
 import metier.EmployeUtil;
 import metier.MaterielUtil;
+import metier.PlanningUtil;
 
 public class Exec {
 
-	public static void main(String[] args) {
-		
-		Employe e = Factory.getEmploye();
-		Materiel m = Factory.getMateriel();
-		Tache t = Factory.getTache();
+	public static void main(String[] args) throws ParseException {
 		
 		EmployeUtil eu = EmployeUtil.getInstance();
 		MaterielUtil mu = MaterielUtil.getInstance();
-		//PlanningUtil pu = PlanningUtil.getInstance();
+		PlanningUtil pu = PlanningUtil.getInstance();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Employe e = Factory.getEmploye();
+		Materiel m = Factory.getMateriel();
+		Tache t = pu.addTache(e, "debrouissailler", "Allee des noisetiers", sdf.parse("2016-06-03"), 1);
 		
 		eu.modifEmploye(e, "Wallace", "William");
 		mu.modifMateriel(m, "scie cirulaire");
 		
+		e.setId(1);
+		m.setIdMateriel(1);
+		
+		
 		System.out.println(e);
 		System.out.println(m);
+		System.out.println(t);
 		
 		
 		
