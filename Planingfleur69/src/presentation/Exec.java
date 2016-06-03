c9f27b1cc0f1dc6b4f59528c7af23d8d13e31f56
leo <killerofgnom@gmail.com>
2016-06-03 13:11:08 +0200

    Etat = true par defaut Debut de l'exec de test

--- Planingfleur69/src/presentation/Exec.java
+++ Planingfleur69/src/presentation/Exec.java
@@ -1,9 +1,32 @@
 package presentation;
 
+import Entite.Employe;
+import Entite.Factory;
+import Entite.Materiel;
+import Entite.Tache;
+import metier.EmployeUtil;
+import metier.MaterielUtil;
+
 public class Exec {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Employe e = Factory.getEmploye();
+		Materiel m = Factory.getMateriel();
+		Tache t = Factory.getTache();
+		
+		EmployeUtil eu = EmployeUtil.getInstance();
+		MaterielUtil mu = MaterielUtil.getInstance();
+		//PlanningUtil pu = PlanningUtil.getInstance();
+		
+		eu.modifEmploye(e, "Wallace", "William");
+		mu.modifMateriel(m, "scie cirulaire");
+		
+		System.out.println(e);
+		System.out.println(m);
+		
+		
+		
 
 	}
 
